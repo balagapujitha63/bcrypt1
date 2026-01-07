3f06ff2bede3caf9067a7c838882f04820e4d388
pujitha <balagapujitha63@gmail.com>
2026-01-07 16:22:15 +0530

    done

---
+++ server3.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const user = require('./model')
+const bcrypt = require('bcrypt')
+const app=express();
+app.use(express.json())
+mongoose.connect('mongodb+srv://balagapujitha63_db_user:J3J4eJCby5V9kL4I@cluster0.nufscab.mongodb.net/').then(() => console.log('connected to db..')).catch(err => console.log(err))
+app.post('/signup',async (req,res) =>{
+    const {username} = req.body
+    const {email} = req.body
+    const {password} = req.body
+    try{
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashed_password = await bcrypt.hash(password,salt)
+        const newUser = new user({username,email,password:hashed_password})
+        await newUser.save()
+        return res.status(200).json(" user signup sucessfully")
+    }
+    catch (err){
+        console.log(err.message)
+    }
+})
+app.listen(3000,()=>console.log('server running on http://localhost:3000'))
